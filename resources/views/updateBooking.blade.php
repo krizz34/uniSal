5939b4a41cb380f2956a165c0254e03f513dc00d
Anand <panandakrishna@gmail.com>
2024-03-21 00:18:00 +0530

    Form dropdown set

--- resources/views/updateBooking.blade.php
+++ resources/views/updateBooking.blade.php
@@ -18,7 +18,7 @@
         </div>
     @endif
     @if (session('exist'))
-        <div class="alert alert-success">
+        <div class="alert alert-danger">
             {{ session('exist') }}
         </div>
     @endif
@@ -37,7 +37,17 @@
         <label for="bookingDate">Booking Date:</label>
         <input type="date" id="bookingDate" name="bookingDate" value="{{ $booking->bookingDate }}" required><br><br>
         <label for="bookingSlot">Booking Slot:</label>
-        <input type="text" id="bookingSlot" name="bookingSlot" value="{{ $booking->bookingSlot }}" required><br><br>
+        <select id="bookingSlot" name="bookingSlot" required>
+            <option value="09:00 am">09:00 am</option>
+            <option value="10:00 am">10:00 am</option>
+            <option value="11:00 am">11:00 am</option>
+            <option value="12:00 pm">12:00 pm</option>
+            <option value="01:00 pm">01:00 pm</option>
+            <option value="02:00 pm">02:00 pm</option>
+            <option value="03:00 pm">03:00 pm</option>
+            <option value="04:00 pm">04:00 pm</option>
+            <option value="05:00 pm">05:00 pm</option>
+        </select>
         <button type="submit">Update</button>
     </form>
 </body>
